193fd7774c113be96e638e36c01d44bda7a8ae53
TomRigger <adityatomar1280@gmail.com>
2025-09-18 23:17:11 +0530

    new commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const chatRoutes = require("./api/chat");
+const chatRoutes = require("./src/api/chat");
 
 const app = express();
 const port = process.env.PORT || 3001;
